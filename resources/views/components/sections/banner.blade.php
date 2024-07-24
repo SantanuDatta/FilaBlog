4db61498740f73ccd05a603dcd8cff1d73bbd11e
SantanuDatta <santanudatta94@gmail.com>
2024-07-24 12:17:47 +0600

    main

--- resources/views/components/sections/banner.blade.php
+++ resources/views/components/sections/banner.blade.php
@@ -1,12 +1,10 @@
-<div class="relative isolate min-h-screen px-6 pt-14 lg:px-8">
-    <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
-        <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%,
-            72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%,
-            17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-        ></div>
+<div class="relative px-6 py-16 lg:px-8 lg:py-8">
+    <div class="relative" aria-hidden="true">
+        <div
+            class="absolute inset-x-0 -top-10 left-10 h-32 w-32 transform-gpu overflow-hidden rounded-full bg-gradient-to-tr from-red-500 via-violet-600 to-indigo-600 blur-3xl filter lg:left-14 lg:top-10 lg:h-56 lg:w-56">
+        </div>
     </div>
-    <div class="mx-auto max-w-2xl py-16 sm:py-24 lg:py-28">
+    <div class="sm:py-30 mx-auto max-w-2xl py-16 lg:py-28">
         <div class="hidden sm:mb-8 sm:flex sm:justify-center">
             <div
                 class="relative rounded-full px-3 py-1 text-sm leading-6 text-slate-600 ring-1 ring-slate-900/10 hover:ring-slate-900/20 dark:text-slate-200 dark:ring-slate-50/50 dark:hover:ring-slate-50/70">
@@ -34,13 +32,9 @@
             </div>
         </div>
     </div>
-    <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-    >
-        <div class="relative left-[calc(50%+3rem)] aspect-[1155/480] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-            style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%,
-            72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%,
-            17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-        ></div>
+    <div class="relative"aria-hidden="true">
+        <div
+            class="absolute inset-y-0 -top-28 right-0 h-32 w-32 transform-gpu overflow-hidden rounded-full bg-gradient-to-tr from-rose-600 via-violet-600 to-blue-600 blur-3xl filter lg:-top-60 lg:right-8 lg:h-56 lg:w-56">
+        </div>
     </div>
 </div>
